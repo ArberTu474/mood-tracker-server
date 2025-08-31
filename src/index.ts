11c1ca9767f8fdea3b12d9b79f7b93d9f7df8687
ArberTu474 <arber.tusha@outlook.com>
2025-08-31 21:53:23 +0200

    changed port

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { ENV } from './config/env'
 
 console.log(ENV.DATABASE_URL)
 
-app.listen(ENV.PORT, () => {
+app.listen(ENV.PORT || 3000, () => {
   console.log(`🚀 Server running on http://localhost:${ENV.PORT}`)
 })
